c0858317ddb5b28589b787ef399bec00883446eb
swimmingkiim <swimmingkiim@gmail.com>
2022-10-18 18:57:23 +0900

    Clear default example code

--- bin/firestore_migration.dart
+++ bin/firestore_migration.dart
@@ -1,5 +1,3 @@
-import 'package:firestore_migration/firestore_migration.dart' as firestore_migration;
-
 void main(List<String> arguments) {
-  print('Hello world: ${firestore_migration.calculate()}!');
+  print('Hello world!');
 }
