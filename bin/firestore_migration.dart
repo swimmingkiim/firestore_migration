f60e5c5b5670615e12fbe735952d6e035398c79b
swimmingkiim <swimmingkiim@gmail.com>
2022-10-18 21:43:14 +0900

    Find version and execute migration code

--- bin/firestore_migration.dart
+++ bin/firestore_migration.dart
@@ -1,14 +1,17 @@
+import 'dart:io';
+
 import 'package:dart_console/dart_console.dart';
+import 'package:dart_eval/dart_eval.dart';
 import 'package:firestore_migration/src/arguments_parser.dart';
 import 'package:firestore_migration/src/util.dart';
 
-void main(List<String> arguments) {
+Future<void> main(List<String> arguments) async {
   final console = Console();
 
   // Parse command line arguments
   final argumentsResult = parseArguments(arguments);
   final command = argumentsResult.command;
-  final version = argumentsResult['version'];
+  String? version = argumentsResult['version'];
   final isHelp = argumentsResult['help'];
 
   // Check isHelp is true
@@ -17,9 +20,44 @@ void main(List<String> arguments) {
     return;
   }
 
+  // Check if a command exists
+  if (command == null) {
+    console.writeLine(
+        'If you want to run migration make sure you add "migrate" command');
+    return;
+  }
+
+  // Start Migration
   console.writeLine('Start : Firestore Migration...');
   console.writeLine('\n');
 
+  final migrationFiles = await getFilesWithVersion(Directory('migrations'));
+
+  late File migrationFile;
+  if (version == null) {
+    // Find latest version
+    version = migrationFiles.last.key.toString();
+    migrationFile = migrationFiles.last.value;
+  } else {
+    // Check if version file exists
+    final fileIndex =
+        migrationFiles.indexWhere((entry) => entry.key.toString() == version);
+    if (fileIndex >= 0) {
+      migrationFile = migrationFiles[fileIndex].value;
+    } else {
+      throw Error();
+    }
+  }
+
+  // Run migration script
+  final rawString = migrationFile.readAsStringSync();
+  eval(
+    rawString,
+    function: 'migrate_${version.replaceAll('.', '_')}',
+    args: [],
+  );
+
+  // End Migration
   console.writeLine('\n');
   console.writeLine('Finish : Firestore Migration!');
 }
