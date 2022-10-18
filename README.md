d1b9c754dbae4dca4b873671cbb0290099b7b98f
swimmingkiim <swimmingkiim@gmail.com>
2022-10-18 18:06:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# firestore_migration
+A Dart console application for firestore migration
---
+++ bin/firestore_migration.dart
@@ -0,0 +1,5 @@
+import 'package:firestore_migration/firestore_migration.dart' as firestore_migration;
+
+void main(List<String> arguments) {
+  print('Hello world: ${firestore_migration.calculate()}!');
+}
