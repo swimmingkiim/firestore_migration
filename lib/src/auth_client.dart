e273b4577aae17ec3fab21605da6900a059af4d5
swimmingkiim <swimmingkiim@gmail.com>
2022-10-19 11:41:39 +0900

    Add getAuthClient

---
+++ lib/src/auth_client.dart
@@ -0,0 +1,15 @@
+import 'package:firebaseapis/identitytoolkit/v1.dart' as firebase_auth;
+import 'package:firebaseapis/firebaserules/v1.dart' as firebase_rules;
+import 'package:firebaseapis/firestore/v1.dart' as firestore;
+import 'package:googleapis_auth/auth_io.dart';
+
+// Ref : https://github.com/google/googleapis.dart/tree/master/googleapis_auth#autonomous-application--service-account
+Future<AutoRefreshingAuthClient> getAuthClient(
+  Map<String, dynamic> serviceAccountJson,
+) async {
+  return await clientViaServiceAccount(
+      ServiceAccountCredentials.fromJson(serviceAccountJson), [
+    firestore.FirestoreApi.cloudPlatformScope,
+    firestore.FirestoreApi.datastoreScope,
+  ]);
+}
