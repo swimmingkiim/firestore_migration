6fa567eef0e3c51e95520e1a51e47e31ed221584
swimmingkiim <swimmingkiim@gmail.com>
2022-10-19 11:39:53 +0900

    Add service account option

--- lib/src/arguments_parser.dart
+++ lib/src/arguments_parser.dart
@@ -4,6 +4,7 @@ ArgResults parseArguments(List<String> arguments) {
   final argParser = ArgParser()
     ..addCommand('migrate')
     ..addOption('version', abbr: 'v')
+    ..addOption('service-account', abbr: 's', mandatory: true)
     ..addFlag('help');
   final result = argParser.parse(arguments);
   return result;
