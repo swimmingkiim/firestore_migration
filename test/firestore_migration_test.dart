ea4e3b7e39d2ef191a990ac4a6e0d74c9067491a
swimmingkiim <swimmingkiim@gmail.com>
2022-10-19 14:37:51 +0900

    Empty default test code

--- test/firestore_migration_test.dart
+++ test/firestore_migration_test.dart
@@ -1,8 +1,5 @@
-import 'package:firestore_migration/firestore_migration.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  test('placeholder', () {});
 }
